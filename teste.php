163c5ce2f2f457d4bb2e7ea65a5096e944589a52
Matheus Moura <matheusmfonseca1@gmail.com>
2019-09-15 20:18:20 -0300

    meu novo commit

---
+++ teste.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<!-- Iniciando um repositorio-->
+
+	<?php 
+		//git init iniciando repositorio
+
+		/*Para monitor o arquivo no git usa:
+
+		git add "nomeDoArquivo"
+
+		comando para adicionar(monitorar) varios arquivos
+
+		git add -A
+		*/
+	?>
+
+</body>
+</html>
