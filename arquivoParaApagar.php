7a6cc52fb1a935c05572e1090128c5e031dd4966
Matheus Moura <matheusmfonseca1@gmail.com>
2019-09-15 21:18:04 -0300

    reset funcionou

---
+++ arquivoParaApagar.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+	<?php //vamos apagar esse arquivo?>
+</body>
+</html>
--- teste.php
+++ teste.php
@@ -17,6 +17,47 @@
 		comando para adicionar(monitorar) varios arquivos
 
 		git add -A
+
+
+		----------------------------------------------------------------------------------------------------
+		
+		Quando usar o git status e verificar o que foi modificado. Caso queira detalhar devemos usar o comando:
+		git diff
+		
+		---------------------------------------------------------------------------------------------------------
+		branch = mesa de trabalho atual
+
+		git log mostra todos os commit's feitos
+
+		---------------------------------------------------
+		git branch mostra as mesas de trabalhos que tenho e qual a atual 
+
+		para mudar de uma mesa para outra usa o git checkout "nomeDaMesa" caso exista é feito a mudança, caso não é criado o novo branch
+
+
+		--------------------------------------------------------------------------------------------------------------------------------------
+
+		colocando o commit em um repositorio publico:
+
+		1-) primeiro devemos validar o computador, para isso vamos criar uma chave ssh com o comando :
+
+		$ ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+
+
+		--------------------------------------------------------------------------------------------------------
+
+		Para fazer o commit de tudo mesmo que não estejam trackeados os arquivos 
+
+		git commit -am "descricao do commit"
+
+		----------------------------------------------------------------------------------------------------------------------------------
+
+		para voltar um backup usamos:
+
+		git reset --hard codigo pego no git log referente ao backup
+
+
+
 		*/
 	?>
 
